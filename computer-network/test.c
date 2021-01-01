d30dd7c594ba06c982e9cd98bb680377b8ed3081
Sarmad Rezayat <sadmad@debian.laptop>
2021-01-01 13:32:14 +0100

    try to add function to the code

--- computer-network/test.c
+++ computer-network/test.c
@@ -2,35 +2,75 @@
 
     Approach to Simplified Data Encryption Standard (S-DES)
     Fallowed the instruction by Prof. William Stallings
-    You find the pdf of it here:
+    You find the pdf here:
     http://mercury.webster.edu/aleshunas/COSC%205130/G-SDES.pdf
+
 
 */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 //initializing the variables
+//--------------------------
+
+int p8[] = {6, 3, 7, 4, 8, 5, 10, 9};        //8 bits of permuted input
+int p1[] = {3, 5, 2, 7, 4, 10, 1, 9, 8, 6};  //10 bit of permuted input
+int ip[] = {2, 6, 3, 1, 4, 8, 5, 7};         //inverse permutation
+int ipRev[] = {4, 1, 3 , 5, 7, 2, 8, 6};     //reverse of inverse permutation
+int n = 0;                                   //used as a permanent holder for LS-2
+int i;                                       //use for for loops
+char inputKey[11];                           //The variable for storing the 10 bit input key from the user
+char temp[11];                               //is a temporary var for all of our calculation 
+char ext[2];                                 //extra variable for permutation
+char k1[9];                                  //first 8 bit key - I added a null at the end of the key
+char k2[9];                                  //second 8 bit key
+
+//............................
 
 
-int p8[] = {6, 3, 7, 4, 8, 5, 10, 9};   //
-int p1[] = {3, 5, 2, 7, 4, 10, 1, 9, 8, 6};
-int n = 0;
-int i;  //use for for loops
-char inputKey[11];  //The variable for storing the 10 bit input key from the user
-char temp[11];      
+//get the input key from user
+void getInput () {
 
-char ext[2];
-char k1[9];  //first 8 bit key - I added a null at the end of the key
-char k2[9];  //second 8 bit key
-    
-int main(){
+    printf("Please enter 10 bits as input it will ask you a bit eachtime(10 times): ");
+    for(i = 0; i < 10; ++i){      
+        
+        printf("%i\n", i);
+        scanf("%i", &n);
+        if(n == 1 || n == 0) { 
+            inputKey[i] = n + '0';
+            /*
+            printf("Please enter the next bit(it will be ");
+            if(i==0){
+                printf("second bit): ");
+            }else if(i=1){ 
+                printf("third bit): ");
+            }else{ 
+                printf("%ith bit): ", i);
+            };
+            */
+        }else{
+            printf("Please choose between 0 and 1!!!");
+            i--;
+        };
 
-    //get the inputKey from user
-    printf("Please enter 10 bits of input:");
-    scanf("%s", &inputKey); 
+    };
+
     inputKey[10] = '\0';
     printf("input: ");
     puts(inputKey);
+    
+};
+
+//get the plain text from user 
+
+int main(){
+
+    //get the inputKey from user
+    getInput();
+    
 
     //starting to build k1
     //applying p1
